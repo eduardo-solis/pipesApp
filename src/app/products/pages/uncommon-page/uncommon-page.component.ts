1e4c44894ba9172d38c33c93743a9be5304cee61
Eduardo Solis <solishernandezeduardoruben@gmail.com>
2024-05-13 18:40:05 -0600

    Usando el pipe json y keyvalue

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -35,4 +35,13 @@ export class UncommonPageComponent {
     this.clients.shift();
   }
 
+  // KeyValue Pipe
+  public person = {
+    name: 'Eduardo',
+    age: 24,
+    address: 'León, México'
+  }
+
+
+
 }
