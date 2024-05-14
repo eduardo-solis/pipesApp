b59987d5cffc947804253d403b69cf72a8dc6504
Eduardo Solis <solishernandezeduardoruben@gmail.com>
2024-05-13 18:51:15 -0600

    Usando el pipe async

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -41,6 +42,21 @@ export class UncommonPageComponent {
     age: 24,
     address: 'León, México'
   }
+
+  // Async Pipe
+
+  public myObservableTimer: Observable<number> = interval(2000).pipe(
+    tap( value => console.log(value)
+     )
+  );
+
+  public promiseValue = new Promise( (resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa')
+      console.log('Tenemos data en la promesa');
+      
+    }, 3500)
+  })
 
 
 
