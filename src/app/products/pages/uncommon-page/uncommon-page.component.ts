e401de8cbd247c21864d99ec30f825ada19cf686
Eduardo Solis <solishernandezeduardoruben@gmail.com>
2024-05-13 18:25:25 -0600

    Usando el pipe i18nPlural

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
-  // i18n Select
+  // i18nSelect
   public name: string = 'Eduardo';
   public gender: 'male'|'female' = 'male';
   public inivationMap = {
@@ -18,6 +18,21 @@ export class UncommonPageComponent {
   changeClient(): void {
     this.name = 'Sandra';
     this.gender = 'female';
+  }
+
+  // i18nPlural
+  public clients: string[] = ['Maria', 'Pedro', 'Eduardo', 'Sandra', 'Uriel',
+                              'Fernando', 'Paco', 'Natalia', 'Nohemi', 'Ulises'];
+
+  public clientMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos 1 cliente esperando.',
+    '=2': 'tenemos 2 esperando.',
+    'other': 'tenemos # clientes esperando.'
+  }
+
+  deleteClient(): void{
+    this.clients.shift();
   }
 
 }
