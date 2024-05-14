33dacda0adcced220ddf8e0b25ac64bdb95fa5e8
Eduardo Solis <solishernandezeduardoruben@gmail.com>
2024-05-13 18:08:16 -0600

    Usando el pipe i18nSelect

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18n Select
+  public name: string = 'Eduardo';
+  public gender: 'male'|'female' = 'male';
+  public inivationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient(): void {
+    this.name = 'Sandra';
+    this.gender = 'female';
+  }
+
 }
